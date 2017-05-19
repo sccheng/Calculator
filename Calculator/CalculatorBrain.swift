815b33880f66f5b78fa5caebd2c556d7fe5c35c9
Stephanie Cheng <stephanieccheng@yahoo.com>
2017-05-19 00:35:29 -0500

    Added constants and unary operations to the CalculatorBrain model

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,20 +8,37 @@
 
 import Foundation
 
+func changeSign(operand: Double) -> Double {
+    return -operand
+}
+
 struct CalculatorBrain {
     
     private var accumulator: Double?
     
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "±": Operation.unaryOperation(changeSign)
+    ]
+    
     mutating func performOperation(_ symbol: String) {
-        switch symbol {
-        case "π":
-            accumulator = Double.pi
-        case "√":
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
             }
-        default:
-            break
         }
     }
     
