f2433ad18909a325f40bf1b317a65fca7651e12b
Stephanie Cheng <stephanieccheng@yahoo.com>
2017-08-05 16:33:36 -0500

    Allow users to type floating point numbers

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
             let textCurrentlyInDisplay = display.text!
-            display.text = textCurrentlyInDisplay + digit
+            if !(digit == "." && textCurrentlyInDisplay.contains(".")) {
+                display.text = textCurrentlyInDisplay + digit
+            }
         } else {
             display.text = digit
             userIsInTheMiddleOfTyping = true
